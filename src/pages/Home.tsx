0535329de713a4da25d1cfb7034cad1e92e484ca
Irina Sorokina <irasoro402@yandex.ru>
2023-03-26 15:46:27 +0400

    New package version

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,46 @@
 import { useState } from 'react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react';
 import { InputDate, InputTime } from '@IraSoro/ionic-datetime-input';
 
-const Home: React.FC = () => {
+const BasicExample = () => {
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
+
+  return (
+    <IonCard>
+      <IonCardHeader>
+        <IonCardTitle color="primary">Basic example</IonCardTitle>
+      </IonCardHeader>
+      <IonCardContent>
+        <InputDate value={date} setValue={setDate} inputID="1" />
+        <InputTime value={time} setValue={setTime} inputID="2" />
+      </IonCardContent>
+    </IonCard>
+  );
+}
+
+const CustomExample = () => {
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+
+  return (
+    <IonCard>
+      <IonCardHeader>
+        <IonCardTitle color="danger">Custom example</IonCardTitle>
+      </IonCardHeader>
+      <IonCardContent>
+        <InputDate value={date} setValue={setDate} inputID="2" isConvert={true} color="danger" title="Russian date" locale="ru" />
+        <InputTime value={time} setValue={setTime} inputID="1" isConvert={true} color="danger" title="Russian time" locale="ru" />
+      </IonCardContent>
+    </IonCard>
+  );
+}
+
+const Home = () => {
   return (
     <>
-      <InputDate value={date} setValue={setDate} color="success" />
-      <InputTime value={time} setValue={setTime} color="success" />
+      <BasicExample />
+      <CustomExample />
     </>
   );
 };
